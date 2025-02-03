26f70979e519de1b2175f0d4da1967f5e71452c9
Alan Rios <alan.rios.hd@gmail.com>
2025-02-03 00:23:38 -0500

    add dotenv configuration and custom 404 error handling

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';  // Agregar dotenv
+
+dotenv.config();  // Cargar las variables de entorno desde el archivo .env
 
 const app = express();
 
@@ -25,7 +28,11 @@ app.get('/form', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'form', 'form.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, 'views', '404', '404.html'));
+});
+
+const PORT = process.env.PORT || 3000;  // Aquí se usa la variable de entorno PORT
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
